a5486fdd706e3dda4744f44a0d0a961f2a3312f4
Ilma Ismail <139671858+IlmaIsmail@users.noreply.github.com>
2024-12-03 17:51:08 +0000

    mapping the product

--- icae1/src/components/Products.js
+++ icae1/src/components/Products.js
@@ -38,6 +38,12 @@ export default function Products(){
                 <div className="grid-container">
                     {
                         //product
+                        flowers.map((flower)=>(
+                            <Product 
+                                key={flower.id}
+                                flower={flower}
+                                onChange={addToCart}/>
+                        ))
                     }
                 </div>
 
